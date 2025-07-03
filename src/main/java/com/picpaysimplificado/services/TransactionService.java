d81f5b36af6a25b7b2b0619464c168dfebeb49c3
ArmandoJunior00 <armando.jr@sofintech.com.br>
2025-07-03 14:54:16 -0300

    feat: create transaction

---
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -0,0 +1,25 @@
+package com.picpaysimplificado.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificado.repositories.TransactionRepository;
+
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired 
+    private TransactionRepository repository;
+
+    public void createTransaction(TransactionDTO transaction) throws Exception{
+        User sender = this.userService.findeUserById(transaction.senderId());
+
+    }
+
+
+}
